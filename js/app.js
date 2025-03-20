908d59301a7de024ee99e7f5ba7c893b6711a2b4
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2025-03-19 23:53:42 -0300

    feat: adiciona função para imprimir no console a quantidade de jogos alugados

--- js/app.js
+++ js/app.js
@@ -1,17 +1,30 @@
+let jogosAlugados = 0;
+
+function contarEExibirJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
 function alterarStatus(id) {
     let jogo = document.getElementById(`game-${id}`);
     let botao = jogo.querySelector('.dashboard__item__button');
     let nomeDoJogo = jogo.querySelector('.dashboard__item__name').textContent;
-
     let status = botao.textContent;
     
     if (status == 'Alugar') {
         botao.textContent = 'Devolver';
         botao.classList.add('dashboard__item__button--return');
+        jogosAlugados++;
     } else {
         if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeDoJogo}?`)) {
             botao.textContent = 'Alugar';
             botao.classList.remove('dashboard__item__button--return');
+            jogosAlugados--;
         }
     }
+    contarEExibirJogosAlugados();
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarEExibirJogosAlugados();
+});

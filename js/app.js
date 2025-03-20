08e8472837f4e3817be63c21f171c411e292caf2
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2025-03-19 23:38:55 -0300

    feat: adiciona confirmação para devolver o jogo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 function alterarStatus(id) {
     let jogo = document.getElementById(`game-${id}`);
     let botao = jogo.querySelector('.dashboard__item__button');
+    let nomeDoJogo = jogo.querySelector('.dashboard__item__name').textContent;
 
     let status = botao.textContent;
     
@@ -8,7 +9,9 @@ function alterarStatus(id) {
         botao.textContent = 'Devolver';
         botao.classList.add('dashboard__item__button--return');
     } else {
-        botao.textContent = 'Alugar';
-        botao.classList.remove('dashboard__item__button--return');
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeDoJogo}?`)) {
+            botao.textContent = 'Alugar';
+            botao.classList.remove('dashboard__item__button--return');
+        }
     }
 }
